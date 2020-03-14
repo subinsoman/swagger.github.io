fd549500e5fd8f94df07b33f550e81585716aa0f
SUBIN SOMAN <tosubins@gmail.com>
2020-03-14 23:52:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 
 function submit() {
     let s = document.getElementById("json_data").value;
-    document.getElementById('yaml_out').value = '';
-    try{
-         let json_object = JSON.parse(s);
-         parse(json_object, '')
-    }catch(err) {
-         document.getElementById('yaml_out').value +=  err.message;
-    }    
-   
+    let path = document.getElementById("path").value;
+    let tag = document.getElementById("tag").value;
+    let model = document.getElementById("model").value;
+    let type = document.getElementById("type").value;
+
+    if (!validateForm(s, path, tag, model, type)) {
+        document.getElementById('yaml_out').value = 'Please Fill All Required Field';
+
+    } else {
+        try {
+            document.getElementById('yaml_out').value = '';
+            let json_object = JSON.parse(s);
+            writeHead(path, tag, model, type);
+            parse(json_object, '    ');
+        } catch (e) {
+            document.getElementById('yaml_out').value = e;
+        }
+    }
 }
 
 function isEmpty(obj) {
@@ -21,6 +31,7 @@ function isEmpty(obj) {
 }
 
 function parse(s, indent) {
+
     if (Array.isArray(s)) {
         write(indent + 'type: \"array\"');
         write(indent + 'items:');
@@ -37,16 +48,63 @@ function parse(s, indent) {
         write(indent + 'properties:');
         for (let key in s) {
             write(indent + '  ' + key + ':');
-            if(s.hasOwnProperty(key)){
+            if (s.hasOwnProperty(key)) {
                 parse(s[key], indent + '    ');
             }
         }
     } else {
-        write(indent + 'type: \"' + (typeof s)+'\"');
+        write(indent + 'type: \"' + (typeof s) + '\"');
     }
 }
 
 function write(s) {
     // console.log(s);
     document.getElementById('yaml_out').value += s + "\n";
+}
+
+function writeHead(path, tag, model, type) {
+
+
+    var header = `swagger: '2.0'
+info:
+  version: 1.0.0
+  title: Swagger Api
+host: 10.0.0.69:8080
+basePath: /OM
+tags:
+  - name: `+ tag + `
+schemes:
+  - https
+  - http
+paths:
+  /`+ path + `:
+    `+ type + `:
+      tags:
+        - `+ tag + `
+      consumes:
+        - application/json
+      produces:
+        - application/json
+      parameters:
+        - in: body
+          name: body
+          required: true
+          schema:
+            $ref: '#/definitions/`+ model + `'
+      responses:
+        '405':
+          description: Invalid input
+definitions:
+  `+ model + `:`;
+
+    write(header);
+
+}
+
+function validateForm(json, path, tag, model, type) {
+    if (json == null || json == "" || path == null || path == "" || tag == null || tag == "" || model == null || model == "" || type == null || type == "") {
+
+        return false;
+    }
+    return true;
 }
